ad78b9a39232eeb3766519f685709b4c6a05bffd
Joshua Bussdieker <jbussdieker@gmail.com>
2012-12-20 23:40:05 -0800

    Default sort order and to_s for simple item

--- app/models/software.rb
+++ app/models/software.rb
@@ -2,4 +2,5 @@ class Software < ActiveRecord::Base
   attr_accessible :name
   has_many :versions, :dependent => :destroy
   accepts_nested_attributes_for :versions, :allow_destroy => :true
+  default_scope order('name')
 end
--- app/models/version.rb
+++ app/models/version.rb
@@ -1,4 +1,9 @@
 class Version < ActiveRecord::Base
   belongs_to :software
   attr_accessible :name
+  default_scope order('name DESC')
+
+  def to_s
+    name
+  end
 end
